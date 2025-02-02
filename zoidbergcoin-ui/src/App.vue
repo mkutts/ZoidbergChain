a24cbf254af1ef61301d537b28dabe14a4b16bd2
mkuttner <matt.kuttner@gmail.com>
2025-02-02 17:32:07 -0500

    set up skelleton UI

---
+++ zoidbergcoin-ui/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/dashboard">Dashboard</router-link>
+      <router-link to="/blockchain">Blockchain</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<style>
+nav {
+  display: flex;
+  gap: 20px;
+  margin-bottom: 20px;
+}
+</style>
